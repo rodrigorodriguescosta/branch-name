fd982c4205194d91c9b996eda266537b8c786fc6
rodrigo <rodrigorodriguescosta@gmail.com>
2020-11-13 11:25:48 -0300

    added variable BRANCH_NAME_LOWER_CASE

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const core = require('@actions/core');
 
 async function run() {
   try {
-    core.exportVariable('BRANCH_NAME', process.env.GITHUB_REF.split('/').pop());
+    let branchName = process.env.GITHUB_REF.split('/').pop();
+    core.exportVariable('BRANCH_NAME', branchName);
+    core.exportVariable('BRANCH_NAME_LOWER_CASE',branchName.toLowerCase());
   }
   catch (error) {
     core.setFailed(error.message);
